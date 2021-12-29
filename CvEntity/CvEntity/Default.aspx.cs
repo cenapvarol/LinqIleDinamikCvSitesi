a869200ffe3eb861c8c2cda04e9fd4dc28521bb8
cenapvarol <varolcenap@gmail.com>
2021-12-29 17:06:29 +0300

    DinamikCvUygulaması

---
+++ CvEntity/CvEntity/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvEntity
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        CbCvEntityEntities db = new CbCvEntityEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+
+            Repeater2.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater2.DataBind();
+
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
+        }
+    }
+}
