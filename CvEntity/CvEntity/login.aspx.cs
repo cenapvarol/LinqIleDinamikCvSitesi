04ddebb6b6fbd1202d1b8f7b55ce1d8579a43775
cenapvarol <varolcenap@gmail.com>
2021-12-31 09:40:50 +0300

    CvUgulaması Güncel Hali

---
+++ CvEntity/CvEntity/login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvEntity
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        CbCvEntityEntities db = new CbCvEntityEntities();
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var sorgu = from x in db.TBLADMIN where x.KULLANICIAD == TextBox1.Text && x.SIFRE == TextBox2.Text select x;
+            if (sorgu.Any())
+            {
+                Response.Redirect("Iletisim.aspx");
+            }
+            else
+            {
+                Response.Write("Hatalı Kullanıcı Adı ya da Şifre");
+            }
+
+        }
+    }
+}
